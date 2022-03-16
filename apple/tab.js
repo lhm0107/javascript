806383890423a6374806a286cfe90e35922d39ed
DESKTOP-N4V80RO\lhm0107 <lhm820107@gmail.com>
2022-03-17 00:14:11 +0900

    java input

--- apple/tab.js
+++ apple/tab.js
@@ -1,7 +1,21 @@
 const tabButton = document.querySelectorAll(".tab-button");
 const tabContent = document.querySelectorAll(".tab-content");
+const buttonList = document.querySelector(".list");
 
-tabButton.entries(0).addEventListener("click", function () {
-	tabButton.classList.remove("orange");
-	tabButton.entries(0).classList.add("orange");
+buttonList.addEventListener("click", function (e) {
+	if (e.target == tabButton[0]) {
+	}
+});
+
+const car = { name: "소나타", price: [50000, 3000, 4000], color: "white" };
+
+document.querySelector(".car-title").innerHTML = car.name;
+document.querySelector(".car-price").innerHTML = car.price[0];
+
+document.querySelectorAll(".form-select")[0].addEventListener("input", function () {
+	if (document.querySelectorAll(".form-select")[0].value == "셔츠") {
+		document.querySelector(".shirt-size").style.display = "block";
+	} else if (document.querySelectorAll(".form-select")[0].value == "모자") {
+		document.querySelector(".shirt-size").style.display = "none";
+	}
 });
